8ea1968fd4183079274d8de206b1d64373f8ae62
Schandroid243 <schadrackngunza@gmail.com>
2022-09-02 09:39:40 +0100

    first commit

---
+++ aspnetapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace aspnetapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
