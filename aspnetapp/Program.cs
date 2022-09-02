72d684ea6e8ac172b122b350f6407eb1909f7cda
Schandroid243 <schadrackngunza@gmail.com>
2022-09-02 10:34:32 +0100

    Update program.cs to recieve injected port by heroku

--- aspnetapp/Program.cs
+++ aspnetapp/Program.cs
@@ -14,6 +14,9 @@ namespace aspnetapp
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+                UseKestrel().
+                UseUrls("http://0.0.0.0:" + Environment.GetEnvironmentVariable("PORT")).
+                Build();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
